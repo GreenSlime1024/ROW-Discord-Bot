612a3843c8380b57c2fc2cca4725b836b9c53dfc
GreenSlime9392 <95684129+GreenSlime9392@users.noreply.github.com>
2022-07-01 12:33:55 +0800

    update

---
+++ cmds/couplets.py
@@ -0,0 +1,24 @@
+import discord
+import random
+from discord.ext import commands
+from core.classes import Cog_Extension
+
+class Couplets(Cog_Extension):
+  @commands.command()
+  async def 貼春聯(self, ctx):
+    '''
+    在頻道名稱後方加上 '"春"或"福"'
+    '''
+    name = ctx.channel.name
+    if name.endswith('⟨春⟩') or name.endswith('⟨福⟩'):
+     await ctx.reply('已經貼過了呦', mention_author=False)
+    else:
+      couplets = random.randint(0, 1)
+      if couplets == 0:
+        await ctx.channel.edit(name=name + '⟨春⟩')
+      else:
+        await ctx.channel.edit(name=name + '⟨福⟩')
+      await ctx.reply('(貼', mention_author=False)
+
+def setup(bot):
+  bot.add_cog(Couplets(bot))
