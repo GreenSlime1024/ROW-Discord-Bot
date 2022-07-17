3d5827c1173e121d4e23d454b8e3ce6ec9df04a7
GreenSlime9392 <95684129+GreenSlime9392@users.noreply.github.com>
2022-07-17 21:12:56 +0800

    update

--- cmds/activity.py
+++ cmds/activity.py
@@ -9,16 +9,10 @@ class Task(Cog_Extension):
     async def activiy_task():
       await self.bot.wait_until_ready()
       while not self.bot.is_closed():
-        await self.bot.change_presence(activity=discord.Game(name="SD預計2022.12.31重啟伺服器"))
-        await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Game(name="運作平台: 桌機"))
-        await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Game(name="Prefix: r!"))
-        await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Game(name="ROW 網站: https://greenslime9392.github.io/posts/21/07/row/"))
-        await asyncio.sleep(10)
-        await self.bot.change_presence(activity=discord.Game(name="Made By GreenSlime#9392"))
-        await asyncio.sleep(10)
+        activity = ['SD預計2022.12.31重啟伺服器', '運作平台: 桌機', 'Prefix: r!', 'ROW 網站: https://greenslime9392.github.io/posts/21/07/row/', 'Made By GreenSlime#9392']
+        for i in range(len(activity)-1):
+          await self.bot.change_presence(activity=discord.Game(name=activity[i]))
+          await asyncio.sleep(10)
         
     self.bg_task = self.bot.loop.create_task(activiy_task())
 
