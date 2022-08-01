18cc7a0d18f7fac7f968a0f63cab6acc36c96c48
GreenSlime9392 <qw24816@gmail.com>
2022-08-01 23:53:12 +0800

    update

--- cmds/activity.py
+++ cmds/activity.py
@@ -12,7 +12,7 @@ class Task(Cog_Extension):
         activity = ['SD預計2022.12.31重啟伺服器', '運作平台: 桌機', 'Prefix: /', 'ROW 網站: https://greenslime9392.github.io/posts/21/07/row/', 'Made By GreenSlime#9392']
         for i in range(len(activity)-1):
           await self.bot.change_presence(activity=discord.Game(name=activity[i]))
-          await asyncio.sleep(10)
+          await asyncio.sleep(15)
         
     self.bg_task = self.bot.loop.create_task(activiy_task())
 
