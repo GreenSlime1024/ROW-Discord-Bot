51efc747ed7020d034dd1f09ce5480f87aaa9bb7
GreenSlime9392 <95684129+GreenSlime9392@users.noreply.github.com>
2022-08-02 15:18:08 +0800

    update

--- cmds/event.py
+++ cmds/event.py
@@ -4,9 +4,6 @@ from discord.ext import commands
 from core.classes import Cog_Extension
 import json
 
-with open('setting.json', mode='r', encoding='utf8') as jfile:
-  jdata = json.load(jfile)
-
 class Event(Cog_Extension):
   @commands.Cog.listener()
   async def on_message(self, msg):
