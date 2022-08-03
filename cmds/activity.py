b19d918ffca301f544f507437a1ea098e1d0f77a
GreenSlime9392 <qw24816@gmail.com>
2022-08-03 23:06:01 +0800

    update

--- cmds/activity.py
+++ cmds/activity.py
@@ -9,7 +9,7 @@ class Task(Cog_Extension):
     async def activiy_task():
       await self.bot.wait_until_ready()
       while not self.bot.is_closed():
-        activity = ['SD預計2022.12.31重啟伺服器', '運作平台: 桌機', 'Prefix: /', 'ROW 網站: https://greenslime9392.github.io/posts/21/07/row/', 'Made By GreenSlime#9392']
+        activity = ['SD預計2022.12.31重啟伺服器', '運作平台: 桌機', '現在改用 slash command (/)', 'ROW 網站: https://greenslime9392.github.io/posts/21/07/row/', 'Made By GreenSlime#9392']
         for i in range(len(activity)-1):
           await self.bot.change_presence(activity=discord.Game(name=activity[i]))
           await asyncio.sleep(15)
