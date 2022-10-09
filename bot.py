5f18efa470cd522d43344b9dd1809dd93c75fe00
GreenSlime1024 <greenslime1024@gmail.com>
2022-10-10 00:54:52 +0800

    update

--- bot.py
+++ bot.py
@@ -10,24 +10,27 @@ with open('not_token.json', mode='r', encoding='utf8') as jfile:
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix='r!', intents=intents)
+bot = commands.Bot(command_prefix='r!', intents=intents, owner_id = 1022080471506624545)
 slash = SlashCommand(bot, sync_commands=True)
 
 @bot.event
 async def on_ready():
-  print('>>Bot is online<<')
+  print('I am running!')
 
 @slash.slash(name='load', description='load')
+@commands.is_owner()
 async def load(ctx, extension):
   bot.load_extension(f'cmds.{extension}')
   await ctx.send(f'已載入 **{extension}** ')
 
 @slash.slash(name='unload', description='unload')
+@commands.is_owner()
 async def unload(ctx, extension):
   bot.unload_extension(f'cmds.{extension}')
   await ctx.send(f'已卸載 **{extension}** ')
 
 @slash.slash(name='reload', description='reload')
+@commands.is_owner()
 async def reload(ctx, extension):
   bot.reload_extension(f'cmds.{extension}')
   await ctx.send(f'已重載 **{extension}** ')
