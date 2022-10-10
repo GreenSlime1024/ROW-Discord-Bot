4d175904858e7321dd7faf8cd23c83268e759d7b
GreenSlime1024 <greenslime1024@gmail.com>
2022-10-10 12:51:54 +0800

    update

--- cmds/admin.py
+++ cmds/admin.py
@@ -10,34 +10,30 @@ with open('admin.json', mode='r', encoding='utf8') as jfile:
 class Admin(Cog_Extension):
     
     @cog_ext.cog_slash()
+    @commands.is_owner()
     async def set_order_channel(self, ctx, channel : discord.TextChannel):
         '''
         設置訂單放置頻道
         '''
-        if ctx.author.id in jdata['admin']:
-            jdata["Order_channel"] = channel.id
-            with open('setting.json', mode='w', encoding='utf8') as jfile:
-                json.dump(jdata, jfile, indent=4)
-            self.channel = self.bot.get_channel(channel.id)
-            await ctx.send(f'已設置訂單儲存頻道: {self.channel.mention}')
-        else:
-            await ctx.send(f'權限不足真可憐')
+        jdata["Order_channel"] = channel.id
+        with open('setting.json', mode='w', encoding='utf8') as jfile:
+            json.dump(jdata, jfile, indent=4)
+        self.channel = self.bot.get_channel(channel.id)
+        await ctx.send(f'已設置訂單儲存頻道: {self.channel.mention}')
 
     @cog_ext.cog_slash()
+    @commands.is_owner()
     async def set_trade_channel(self, ctx, channel : discord.TextChannel):
         '''
         設置物品上架頻道
         '''
-        if ctx.author.id in jdata['admin']:
-            with open('setting.json', mode='r', encoding='utf8') as jfile:
-                jdata = json.load(jfile)
-            jdata["Trade_channel"] = channel.id
-            with open('setting.json', mode='w', encoding='utf8') as jfile:
-                json.dump(jdata, jfile, indent=4)
+        with open('setting.json', mode='r', encoding='utf8') as jfile:
+            jdata = json.load(jfile)
+        jdata["Trade_channel"] = channel.id
+        with open('setting.json', mode='w', encoding='utf8') as jfile:
+            json.dump(jdata, jfile, indent=4)
             self.channel = self.bot.get_channel(channel.id)
             await ctx.send(f'已設置物品上架頻道: {self.channel.mention} ')
-        else:
-            await ctx.send(f'權限不足真可憐')
 
 def setup(bot):
     bot.add_cog(Admin(bot))
